d4b606b180f9db172226b8db0b7276b9770772dc
rnakkas <rahman.nakkas@gmail.com>
2024-09-19 14:14:22 +1000

    removed level one
    removed level_one and added level_test

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _on_play_button_pressed() -> void:
 	get_tree().paused = false
-	get_tree().change_scene_to_packed(Global.LEVEL_ONE)
+	get_tree().change_scene_to_packed(Global.LEVEL_TEST)
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
