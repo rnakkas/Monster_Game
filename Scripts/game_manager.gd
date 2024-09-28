71a67388a5cb356fb2475dd92f863a2625c4d2e7
rnakkas <rahman.nakkas@gmail.com>
2024-09-28 17:55:25 +1000

    added key
    - added key collectible
    - created game manager to track key status

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,11 +1,8 @@
 extends Node
 
+var has_key: bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _update_key() -> void:
+	has_key = true
+	print(has_key)
+	
---
+++ Scripts/key.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var game_manager: Node = %game_manager
+@onready var sprite: AnimatedSprite2D = $sprite
+
+func _ready() -> void:
+	sprite.play("idle")
+
+# When player enter area, collect key and update key status
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		game_manager._update_key()
+		var tween_one : Tween = get_tree().create_tween()
+		var tween_two : Tween = get_tree().create_tween()
+		tween_one.tween_property(self, "position", position - Vector2(0, 17), 0.4)
+		tween_two.tween_property(self, "modulate:a", 0, 0.5)
+		tween_one.tween_callback(queue_free)
+		
