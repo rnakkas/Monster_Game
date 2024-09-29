32142b39251a7e1ee078b871d1489d11c5245a12
rnakkas <rahman.nakkas@gmail.com>
2024-09-29 19:18:58 +1000

    added door
    added door, logic and animation

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,8 +1,10 @@
 extends Node
 
 var has_key: bool = false
+var chest_opened: bool = true
 
 func _update_key() -> void:
 	has_key = true
-	print(has_key)
-	
+
+func _update_chest_opened() -> void:
+	chest_opened = true
